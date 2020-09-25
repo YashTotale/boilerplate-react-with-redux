a0ca4be8bf61e63786b9db3ce7713ef41055a508
YashTotale <totaleyash@gmail.com>
2020-09-25 13:59:37 -0700

    body is 100% height

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,12 +4,14 @@ import React from "react";
 //Material UI Imports
 import { makeStyles, Theme } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme: Theme) => ({}));
+const useStyles = makeStyles((theme: Theme) => ({
+  home: {},
+}));
 
 const HomePage: React.FC = () => {
   const classes = useStyles();
 
-  return <div>Home</div>;
+  return <div className={classes.home}>Home</div>;
 };
 
 export default HomePage;
