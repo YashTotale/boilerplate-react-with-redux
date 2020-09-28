56df83f905e72c3b904937b94a47fc865c949b14
YashTotale <totaleyash@gmail.com>
2020-09-28 13:55:59 -0700

    changed thunk example

--- src/Redux/thunks/index.ts
+++ src/Redux/thunks/index.ts
@@ -1,15 +1,21 @@
 //Redux Thunks
 
-/* 
+/*
 
 Example:
 
-export const testThunk = () => async (dispatch, getState) => {
+import { Dispatch } from "react";
+import { RootStateOrAny } from "react-redux";
+import { AnyAction } from "redux";
+
+export const testThunk = () => async (
+  dispatch: Dispatch<AnyAction>,
+  getState: () => RootStateOrAny
+) => {
   try {
     dispatch(aLoadingAction());
     const response = await fetch(aURL);
     const json = await response.json();
-
     dispatch(aSuccessAction(json));
   } catch (e) {
     dispatch(aFailureAction(e));
